96af76a4ca07735414187baeb906ddb0bc033c26
VIctor <colen.victor@icloud.com>
2021-12-14 16:06:54 -0300

    Initial Commit

---
+++ iHabit/iHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHabitApp.swift
+//  iHabit
+//
+//  Created by Victor Colen on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
